1a1b6fc0d7259c167690408f308f4e7a4363f471
Luke Shea <shealuke@yahoo.com>
2023-07-05 00:58:44 -0500

    Managed to fix the bug, now it will fetch data upon click

--- advice_generator/src/Advice.jsx
+++ advice_generator/src/Advice.jsx
@@ -1,12 +1,12 @@
-const Advice = ({ id, advice }) => {
+const Advice = ({ id, advice, fetchData }) => {
     return (
         <>
-            <h5>Advice #117</h5>
+            <h5>Advice #{id}</h5>
             <p key={id}>"{advice}"</p>
             <div>
                 <i className="fa-solid fa-pause pause"></i>
             </div>
-            <button type="button" className="btn">
+            <button type="button" className="btn" onClick={() => fetchData()}>
                 <i className="fa-solid fa-dice-five dice"></i>
             </button>
         </>
--- advice_generator/src/App.jsx
+++ advice_generator/src/App.jsx
@@ -7,30 +7,25 @@ const url = "https://api.adviceslip.com/advice";
 
 function App() {
     const [advice, setAdvice] = useState("");
-    window.addEventListener("DOMContentLoaded", () => {
-        const btn = document.querySelector(".btn");
 
-        btn.addEventListener("click", () => {
-            const fetchData = async () => {
-                try {
-                    const resp = await fetch(url);
-                    const data = await resp.json();
-                    const slip = data.slip;
-                    console.log(slip);
-                    setAdvice(slip);
-                } catch (error) {
-                    console.log(error);
-                }
-            };
-            fetchData();
-            console.log("hello");
-        });
-    });
+    const fetchData = async () => {
+        try {
+            const resp = await fetch(url);
+            const data = await resp.json();
+            const slip = data.slip;
+            setAdvice(slip);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    useEffect(() => {
+        fetchData();
+    }, []);
 
     return (
         <>
             <main>
-                <Advice {...advice} />
+                <Advice {...advice} fetchData={fetchData} />
             </main>
         </>
     );
