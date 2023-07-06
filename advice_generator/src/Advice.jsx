51fd4da62660e9df5b1d911bda2dcacacd4ef4f6
Luke Shea <shealuke@yahoo.com>
2023-07-05 22:44:09 -0500

    Finally finished this stupid thing. As much as I'm going to finish it, anyways.

--- advice_generator/src/Advice.jsx
+++ advice_generator/src/Advice.jsx
@@ -3,8 +3,14 @@ const Advice = ({ id, advice, fetchData }) => {
         <>
             <h5>Advice #{id}</h5>
             <p key={id}>"{advice}"</p>
-            <div>
+            <div className="divider">
+                <div className="left-bar">
+                    <div className="bar"></div>
+                </div>
                 <i className="fa-solid fa-pause pause"></i>
+                <div className="right-bar">
+                    <div className="bar"></div>
+                </div>
             </div>
             <button type="button" className="btn" onClick={() => fetchData()}>
                 <i className="fa-solid fa-dice-five dice"></i>
